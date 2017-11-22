f6a92a592f79e070c1b5d0a679f3d318c5e8494d
rsirving <rstephenirving@gmail.com>
2017-11-22 09:51:10 -0500

    November 22, 2017. Graduation!

--- MEAN/survey_form/server.js
+++ MEAN/survey_form/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', __dirname + "/views");
@@ -11,6 +12,18 @@ app.post('/results', function(request, response){
     console.log("POST DATA \n\n", request.body)
     response.render('results', {title: "Results", name: request.body.name, location: request.body.location, language: request.body.language, comment: request.body.comment});
 })
-app.listen(8000, function(){
+var server = app.listen(8000, function(){
     console.log("Listening on port 8000");
+});
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket){
+    console.log("Client/socket is connected!");
+    console.log("Client/socket id is: ", socket.id);
+    socket.on("posting_form", function(data){
+        let dataString = JSON.stringify(data);
+        let message = "You emitted the following information to the server: " + dataString;
+        socket.emit('server_response', {response: message});
+        let randnumber = Math.floor(Math.random()*1000);
+        socket.emit('lucky_number', {number: randnumber});
+    })
 })
--- Python/python_fundamentals/requests_function.py
+++ Python/python_fundamentals/requests_function.py
@@ -1,13 +1,3 @@
 import requests
-r = requests.post('http://httpbin.org/post', data = {'key':'value'})
-print r
-payload = {'key1': 'value1', 'key2': 'value2'}
-r = requests.get('http://httpbin.org/get', params=payload)
-print(r.url)
-# def basic_info(url):
-#     print "test"
-#     info = r.get(url)
-#     print info
-#     print info.json
-
-# basic_info('https://www.reddit.com')
+r = requests.get('https://www.google.com')
+print r.content
